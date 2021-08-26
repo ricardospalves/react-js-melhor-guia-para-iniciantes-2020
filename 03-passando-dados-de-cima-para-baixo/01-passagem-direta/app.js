a410d5f7a6414eac029d80cae8db32e4de9b619f
ricardospalves <ricardospalves@gmail.com>
2021-08-26 09:53:12 -0300

    Adicionar comentários no app.js da Passagem Direta

--- 03-passando-dados-de-cima-para-baixo/01-passagem-direta/app.js
+++ 03-passando-dados-de-cima-para-baixo/01-passagem-direta/app.js
@@ -1,6 +1,18 @@
 (function() {
   'use strict'
 
+  /**
+   * A passagem direta de dados é a forma mais simples que existe de propagar
+   * informação do componente pai pra o componente filho, portanto, o componente
+   * que irá receber a informação precisa ser filho do componente que vai enviar
+   * a informação.
+   *
+   * No meu caso eu quero propagar a informação que está no Component1 para o
+   * Component4, então eu terei que obrigatóriamente passar a informação por
+   * todos os componente pai do Component4 até chegar nele. Então o fluxo da
+   * informação será Component1 > Component2 > Component3 > Component4.
+   */
+
   function Component1() {
     /**
      * A constante myName será enviada pelo Component1 e irá percorrer por todos
@@ -13,21 +25,22 @@
         // O dado que é enviado entre os componentes deve ser um objeto e será
         // passado como argumento do segundo parâmetro do método createElement.
         // No caso abaixo, a constante myName está sendo enviada para o
-        // Component2.
+        // Component2. Por baixo dos panos o que o React vai fazer é pegar o
+        // segundo parâmetro e passar como argumento da função Component2.
         React.createElement(Component2, { myName })
       )
     )
   }
 
   /**
-   * Por convenção, o segundo parâmetro do método createElement é chamado de
-   * props, mas poderia ter qualquer outro nome.
+   * A função Component2 irá receber como parâmetro as propriedades que o
+   * Component1 está propagando. Por convenção, o argumento que um componente
+   * React recebe é chamado de props, mas poderia ser qualquer outro nome.
    *
    * @param {Object} props
    */
   function Component2(props) {
     return (
-      // A div é pai do Component3
       React.createElement('div', { className: 'component-2' },
         // Aqui o Component2 recebe o props do Component1 e o reenvia para o
         // Component3
